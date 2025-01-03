cc03ca1f55028264cd634f2442b861f18ed3464c
adeelehsan <aadeel.ehsan@gmail.com>
2025-01-04 02:23:01 +0500

    update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * as Vectara from "./api";
-export { WrappedVectaraClient as VectaraClient } from "./wrapper/client"
+export { VectaraClient } from "./wrapper/client"
 export { VectaraEnvironment, VectaraEnvironmentUrls } from "./environments";
 export { VectaraError, VectaraTimeoutError } from "./errors";
